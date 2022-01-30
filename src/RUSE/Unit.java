550b1d1181f049dd344f032e33db2015073f1ab5
Sam <samdroid2002@gmail.com>
2022-01-30 17:12:36 -0500

    added getters and setters for Unit class

--- src/RUSE/Unit.java
+++ src/RUSE/Unit.java
@@ -1,5 +1,72 @@
 package RUSE;
 
-public class Unit {
+abstract class Unit {
 
+	private double health;
+	private double maxHealth;
+	private Weapon[] weapons;
+	private Sprite icon;
+	private int buildCost;
+	private int buildTime;
+	private boolean hidden;
+
+	public Unit() {
+
+	}
+
+	public double getHealth() {
+		return health;
+	}
+
+	public void setHealth(double health) {
+		this.health = health;
+	}
+
+	public double getMaxHealth() {
+		return maxHealth;
+	}
+ 
+	public void setMaxHealth(double maxHealth) {
+		this.maxHealth = maxHealth;
+	}
+	
+	public Weapon[] getWeapons() {
+		return weapons;
+	}
+	
+	public void setWeapons(Weapon[] weapons) {
+		this.weapons = weapons;
+	}
+	
+	public Sprite getIcon() {
+		return icon;
+	}
+	
+	public void setIcon(Sprite Icon) {
+		this.icon = Icon;
+	}
+	
+	public int getBuildCost() {
+		return buildCost;
+	}
+	
+	public void setBuildCost(int buildCost) {
+		this.buildCost
+	}
+
+	public int getBuildTime() {
+		return buildTime;
+	}
+	
+	public void setBuildTime(int buildTime) {
+		this.buildTime = buildTime;
+	}
+	
+	public boolean getHidden() {
+		return hidden;
+	}
+	
+	public void setHidden(boolean hidden) {
+		this.hidden = hidden;
+	}
 }
