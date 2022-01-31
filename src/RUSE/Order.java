5ff553e88df7c7919dc45266b0fc95c57b825702
Daniel Peacock <acd19dp@sheffield.ac.uk>
2022-01-31 22:40:01 +0000

    added vecmath jar added setters and getters to weapon class

--- src/RUSE/Order.java
+++ src/RUSE/Order.java
@@ -3,7 +3,6 @@ package RUSE;
 public class Order {
 	
 	
-
 	public Order() {
 
 	}
--- src/RUSE/Weapon.java
+++ src/RUSE/Weapon.java
@@ -17,4 +17,35 @@ public abstract class Weapon {
 	public Rotator getWorldRotation() {
 		return Rotator.add(owner.getRotation(), rotationOffset);
 	}
+	
+	public Rotator getRotationOffset() {
+		return rotationOffset;
+	}
+	
+	public void setRotationOffset(Rotator offset) {
+		this.rotationOffset = offset;
+	}
+	
+	public void setWorldRotation(Rotator rotation) {
+		this.rotationOffset = Rotator.sub(rotation, owner.getRotation());
+	}
+	
+	public Vector2d getPositionOffset() {
+		return positionOffset;
+	}
+	
+	public Point2d getWorldPosition() {
+		Point2d temp = owner.getPosition();
+		temp.add(positionOffset);
+		return temp;
+	}
+	
+	public void setPositionOffset(Tuple2d offset) {
+		this.positionOffset = (Vector2d) offset;
+	}
+	
+	public void setWorldPosition(Tuple2d position) {
+		position.add(positionOffset);
+		this.positionOffset = (Vector2d) position;
+	}
 }
